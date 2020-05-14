3871300442cf61df4393d5847f5d483c64004c47
JuliaSemakhina <47063920+JuliaSemakhina@users.noreply.github.com>
2020-05-14 14:15:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,148 @@
+window.addEventListener('load', () => {
+	let long  ;
+	let lat ;
+	let temperatureDescription = document.querySelector('.temperature-description');
+	let temperatureDegree = document.querySelector('.temperature-degree');
+	let locationTimezone = document.querySelector('.location-timezone');
+	const temperatureSection = document.querySelector('.temperature');
+	const temperatureSpan = document.querySelector('.temperature span');
+
+// const req = new XMLHttpRequest();
+// req.open('GET', 'https://gist.githubusercontent.com/tbranyen/62d974681dea8ee0caa1/raw/3405bfb2a76b7cbd90fde33d8536f0cd13706955/icons.json');
+// req.send();
+// req.onload = function(){
+// 	const weatherIcons = JSON.parse(req.responseText);
+	
+
+
+
+	if(navigator.geolocation){
+
+
+		navigator.geolocation.getCurrentPosition(position =>{
+			lat = position.coords.latitude;
+			long = position.coords.longitude;
+
+		
+		
+
+		const proxy = 'https://cors-anywhere.herokuapp.com/';	
+		const api = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${long}&key=6450498818484b008e70dc92cde21971`;
+		
+
+		fetch(api)
+		.then(res => res.json())
+		.then(data => {
+			console.log(data);
+
+		
+		
+
+			const temp = data.data[0].temp;
+			const description = data.data[0].weather.description;
+			const location = data.data[0].city_name;
+			const icon = data.data[0].weather.description;
+			var result = icon.substr(icon.indexOf(" ") + 1);
+
+			//FORMULA for Celcius
+			// let celcius = (temp-32)*( 5 / 9);
+
+			console.log(result);			
+
+			//Set DOM Elements
+			temperatureDegree.textContent = temp;
+			temperatureDescription.textContent = description;
+			locationTimezone.textContent = location;
+
+			
+			var iconcode = data.data[0].weather.icon;
+
+		   var iconurl = `https://www.weatherbit.io/static/img/icons/${iconcode}.png`;
+		   document.getElementById('wicon').setAttribute('src', iconurl);
+
+
+		
+
+		 //Changethe background depending on daytime
+		var time = new Date();
+        time = time.getHours();
+        var color =  "";
+        if (time >= 19 || time <= 4) {
+            color = "linear-gradient(to top, #3f51b1 0%, #5a55ae 13%, #7b5fac 25%, #8f6aae 38%, #a86aa4 50%, #cc6b8e 62%, #f18271 75%, #f3a469 87%, #f7c978 100%)";
+        } else if (time >= 17) {
+            color = "linear-gradient(to top, #0c3483 0%, #a2b6df 100%, #6b8cce 100%, #a2b6df 100%)";
+        } else if (time >= 7) {
+            color = "linear-gradient(120deg, #89f7fe 0%, #66a6ff 100%)";
+        } else {
+            color = "linear-gradient(to top, #09203f 0%, #537895 100%);";
+        };
+        
+        document.querySelector('body').style.background = color;
+
+
+		     //Change to Celcius
+		// temperatureSection.addEventListener('click', ()=>{
+			
+			
+		// 	if(temperatureSpan.textContent ==="F"){
+		// 		temperatureSpan.textContent ="C";
+		// 		temperatureDegree.textContent = Math.floor(celcius);
+
+		// 	} else {
+		// 		temperatureSpan.textContent = "F";
+		// 		temperatureDegree.textContent = temp;
+		// 	}
+		// });
+
+		  // function setIcons(icon, iconID){
+		  	const skycons = new Skycons({color: "white"});
+		  	
+		  	const docID = document.getElementById("icon2");
+		  	
+		
+		  switch (result) {
+		  	//If clouds
+		  	case 'clouds':
+		  	if (time >=19 || time <=4) {
+		  		skycons.add(docID, Skycons.PARTLY_CLOUDY_NIGHT);
+		  	} else {
+		  		skycons.add(docID, Skycons.PARTLY_CLOUDY_DAY);
+		  	}
+		  	break;
+		  	//If rain
+		  	case 'rain':
+		  	skycons.add(docID, Skycons.RAIN);
+		  	break;
+		  	//If snow
+		  	case 'snow':
+		  	skycons.add(docID, Skycons.SNOW);
+		  	break;
+		  	//IF clear
+		  	case 'sky':
+		  	if (time >=19 || time <=4) {
+		  		skycons.add(docID, Skycons.CEAR_NIGHT);
+		  	} else {
+		  		skycons.add(docID, Skycons.CLEAR_DAY);
+		  	}
+		  	break;
+		  	//If fog
+		  	case 'fog':
+		  	skycons.add(docID, Skycons.FOG);
+		  	break;
+
+		  	default:
+		  	skycons.add(docID, Skycons.CLOUDY);
+		  }
+
+
+  	skycons.play();
+
+
+		
+
+
+		});
+		
+	})};
+
+});
